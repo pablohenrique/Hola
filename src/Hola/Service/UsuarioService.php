643a0a7921b7f92eceff20e804c4dd938b9e88ac
Pablo Henrique <pablohenrique@mint>
2013-07-04 10:09:05 -0300

    verificando usuario e senha contra sqlinjection

--- src/Hola/Service/UsuarioService.php
+++ src/Hola/Service/UsuarioService.php
@@ -24,12 +24,22 @@ class UsuarioService {
 		return $this->usuario;
 	}
 
+	private function removeSpecialCharacters($input){
+		$result;
+		preg_match_all("/[a-z0-9]/i", $cobaia, $match);
+		foreach($match[0] as $value)
+			$result .= $value;
+		return $result;
+	}
+
 	public function __construct(){
 		$this->dao = Factory::getFactory(FACTORY::PGSQL)->getUsuarioDAO();
 	}
 
 	public function post($login, $senha, $email, $celular, $oauth_uid, $oauth_provider, $twitter_oauth_token, $twitter_oauth_token_secret){
-		$this->dao->post(self::createObject($login, $senha, $email, $celular, $oauth_uid, $oauth_provider, $twitter_oauth_token, $twitter_oauth_token_secret));
+		if($login == $senha)
+			throw new Exception("Login and Password MUST be NOT equal.");
+		$this->dao->post(self::createObject(self::removeSpecialCharacters($login), self::removeSpecialCharacters($senha), $email, $celular, $oauth_uid, $oauth_provider, $twitter_oauth_token, $twitter_oauth_token_secret));
 		unset($this->usuario);
 	}
 
